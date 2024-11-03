46cf0ca6c9a0a43158fecfac373a881f8da533c6
FazeelRafiq <malikfazeel@gmail.com>
2024-11-03 23:36:42 +0500

    fix export issue

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext as _
 from import_export.resources import ModelResource
-from import_export.admin import ExportActionMixin
+from import_export.admin import ExportMixin
 from import_export.formats.base_formats import CSV, XLSX
 
 from .models import Client
@@ -12,8 +12,13 @@ class ClientResource(ModelResource):
         # Set the model to be used for the resource
         model = Client
 
-        # Exclude the 'id' and 'created_at' field from export
-        exclude = ('id', 'created_at')
+        # Exclude following fields from export
+        exclude = (
+            'id', 'added', 'tracker_activation_date',
+            'tracker_status_note', 'sim_active', 'sim_status_note',
+            'sim_code', 'sage_invoice_reference', 'sage_payment_reference',
+            'description', 'created_at'
+        )
 
         # Set export field order
         export_order = (
@@ -22,19 +27,25 @@ class ClientResource(ModelResource):
             'tracker_model', 'sold_by', 'country', 'status'
         )
 
-    def get_export_headers(self):
+    def get_export_headers(self, fields=None, selected_fields=None):
         """
         Returns a list of headers to be used in the export file.
         Uses the verbose name of each field in the export fields.
         """
         headers = []
-        for field in self.get_export_fields():
+        # If selected_fields is provided, use it; otherwise, use all export fields
+        if selected_fields:
+            fields = selected_fields
+        else:
+            fields = self.get_export_fields()
+
+        for field in fields:
             # Get the verbose name of the field from the model's meta data
-            headers.append(self.Meta.model._meta.get_field(field.column_name).verbose_name)
+            headers.append(self.Meta.model._meta.get_field(field).verbose_name)
         return headers
 
 
-class ClientAdmin(ExportActionMixin, admin.ModelAdmin):
+class ClientAdmin(ExportMixin, admin.ModelAdmin):
     # Set export formats
     formats = [CSV, XLSX]
 
