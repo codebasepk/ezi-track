464500dc663985d5d07977ab5563949954f68faf
FazeelRafiq <malikfazeel@gmail.com>
2024-11-26 21:40:03 +0500

    add red suspended for tracker status

--- client/static/js/custom.js
+++ client/static/js/custom.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     $("#result_list td.field-status:contains('SUSPENDED')").css("background-color", "red");
+    $("#result_list td.field-tracker_status:contains('SUSPENDED')").css("background-color", "red");
   });
